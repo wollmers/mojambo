d6c8f5ff0a49363d8dda26da88afbb58b905a623
helmut <helmut@acerone.woll.internal>
2012-12-17 00:42:52 +0100

    sortable example

---
+++ lib/Mojambo.pm
@@ -0,0 +1,19 @@
+package Mojambo;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer');
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('pages#index');
+  $r->get('/pages/:name')->to('pages#page');
+}
+
+1;
